2d502fa76a304187ef9cd014dba7ab061b9657b9
Kiril <prestap10@gmail.com>
2025-12-03 12:45:15 +0200

    Fix send receive test case

--- tests/test_orchestration.py
+++ tests/test_orchestration.py
@@ -17,63 +17,66 @@ def _build_block(blockchain: Blockchain, height: int) -> Block:
         proposer_id="validator-1",
     )
     
-def test_send_and_receive_block_blocks(tmp_path):
-    os.remove("data/test1/chain.json") if os.path.exists("data/test1/chain.json") else None
-    os.remove("data/test2/chain.json") if os.path.exists("data/test2/chain.json") else None
-    blockchain1 = Blockchain(data_dir="data/test1")
-    blockchain2 = Blockchain(data_dir="data/test2")
-    os.remove("data/test1/chain.json") if os.path.exists("data/test1/chain.json") else None
-    os.remove("data/test2/chain.json") if os.path.exists("data/test2/chain.json") else None
-    
-    block = _build_block(blockchain1, height=1)
-    assert blockchain1.add_block(block)
-    
-    peers = [
-        {
-            "hostname": "svm-11.cs.helsinki.fi", 
-            "port": 8090
-        },
-        {
-            "hostname": "svm-11.cs.helsinki.fi", 
-            "port": 8091
-        },
-    ]
-    
-    config_1 = Config(config_path="config.yaml")
-    config_1['network']['peers'] = peers
-    config_1.config['node']['port'] = 8090
-    
-    config_2 = Config(config_path="config.yaml")
-    config_2['network']['peers'] = peers
-    config_2.config['node']['port'] = 8091
-    
-    node_1 = Node(config_1)
-    node_2 = Node(config_2)
-    
-    t1 = threading.Thread(target=node_1.start, daemon=True)
-    t2 = threading.Thread(target=node_2.start, daemon=True)
-    
-    t1.start()
-    t2.start()
-    
-    time.sleep(1.5)
-    
-    node_1.blockchain = blockchain1
-    node_2.blockchain = blockchain2
-    
-    node_1_blocks = node_1.blockchain.get_block_headers(1, 2)
-    node_2_address = f"{node_2.network.hostname}:{node_2.network.port}"
-    
-    node_2_height_before = len(node_2.blockchain.chain)
-    
-    node_1.network.send_block(node_1_blocks, node_2_address)
-    
-    time.sleep(1.5)
-    
-    assert len(node_2.blockchain.chain) != node_2_height_before
-    
-    node_1.stop()
-    node_2.stop()
-    
-    os.remove("data/test1/chain.json") if os.path.exists("data/test1/chain.json") else None
-    os.remove("data/test2/chain.json") if os.path.exists("data/test2/chain.json") else None
+def test_send_and_receive_blocks():
+    try:
+        os.remove("data/test1/chain.json") if os.path.exists("data/test1/chain.json") else None
+        os.remove("data/test2/chain.json") if os.path.exists("data/test2/chain.json") else None
+        blockchain1 = Blockchain(data_dir="data/test1")
+        blockchain2 = Blockchain(data_dir="data/test2")
+        
+        block = _build_block(blockchain1, height=1)
+        assert blockchain1.add_block(block)
+        
+        peers = [
+            {
+                "hostname": "svm-11.cs.helsinki.fi", 
+                "port": 8090
+            },
+            {
+                "hostname": "svm-11.cs.helsinki.fi", 
+                "port": 8091
+            },
+        ]
+        
+        config_1 = Config(config_path="config.yaml")
+        config_1['network']['peers'] = peers
+        config_1.config['node']['port'] = 8090
+        
+        config_2 = Config(config_path="config.yaml")
+        config_2['network']['peers'] = peers
+        config_2.config['node']['port'] = 8091
+        
+        node_1 = Node(config_1)
+        node_2 = Node(config_2)
+        
+        t1 = threading.Thread(target=node_1.start, daemon=True)
+        t2 = threading.Thread(target=node_2.start, daemon=True)
+        
+        t1.start()
+        t2.start()
+        
+        time.sleep(1.5)
+        
+        node_1.blockchain = blockchain1
+        node_2.blockchain = blockchain2
+        
+        node_1_blocks = node_1.blockchain.get_block_headers(1, 2)
+        node_2_address = f"{node_2.network.hostname}:{node_2.network.port}"
+        
+        node_2_height_before = len(node_2.blockchain.chain)
+        
+        node_1.network.send_block(node_1_blocks, node_2_address)
+        
+        time.sleep(1.5)
+        
+        assert len(node_2.blockchain.chain) != node_2_height_before
+        
+        node_1.stop()
+        node_2.stop()
+        
+        os.remove("data/test1/chain.json") if os.path.exists("data/test1/chain.json") else None
+        os.remove("data/test2/chain.json") if os.path.exists("data/test2/chain.json") else None
+    except Exception as e:
+        os.remove("data/test1/chain.json") if os.path.exists("data/test1/chain.json") else None
+        os.remove("data/test2/chain.json") if os.path.exists("data/test2/chain.json") else None
+        raise e
