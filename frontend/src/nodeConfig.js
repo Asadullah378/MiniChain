74e018de9e928e60e1477bfb8b9bde90c56b33cc
Asadullah378 <asadnawaz378@gmail.com>
2025-12-09 17:52:43 +0200

    added 4 node branch

--- frontend/src/nodeConfig.js
+++ frontend/src/nodeConfig.js
@@ -2,5 +2,5 @@ export const nodes = [
     { id: 'node1', name: 'Node 1', url: 'http://localhost:8000' },
     { id: 'node2', name: 'Node 2', url: 'http://localhost:8001' },
     { id: 'node3', name: 'Node 3', url: 'http://localhost:8002' },
-    // { id: 'node4', name: 'Node 4', url: 'http://localhost:8003' },
+    { id: 'node4', name: 'Node 4', url: 'http://localhost:8003' },
 ];
